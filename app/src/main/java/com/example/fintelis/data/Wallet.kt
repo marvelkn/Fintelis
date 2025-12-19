b6cf2aa96959bd2f0a6ed4a916a2e48a658e9a0b
VarrelSinaga <varrelnsinaga@gmail.com>
2025-12-19 19:00:05 +0700

    Fixes Dashboard

--- app/src/main/java/com/example/fintelis/data/Wallet.kt
+++ app/src/main/java/com/example/fintelis/data/Wallet.kt
@@ -5,6 +5,5 @@ import com.google.firebase.firestore.DocumentId
 data class Wallet(
     @DocumentId val id: String = "",
     val name: String = "",
-    // This balance is for display and will be calculated, not stored directly in Firestore
     val balance: Double = 0.0
 )
